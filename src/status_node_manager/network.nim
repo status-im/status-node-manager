40543e37064bbf16eb847a64fbc4dcfd322779de
Emil Ivanichkov <emil.ivanichkov@gmail.com>
2024-03-06 15:15:15 +0200

    feat(SNM config): Add `rest` config params

---
+++ src/status_node_manager/network.nim
@@ -0,0 +1,9 @@
+import confutils/std/net
+
+const
+  defaultSNMRestPort* = 13000
+  defaultSNMRestPortDesc* = $defaultSNMRestPort
+
+  defaultAdminListenAddress* = (static parseIpAddress("127.0.0.1"))
+  defaultAdminListenAddressDesc* = $defaultAdminListenAddress
+
